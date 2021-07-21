ba9d835f8cb9720e53adc260a6c381348df9ef1f
Aman Raj <bapjiradx@gmail.com>
2021-07-21 09:13:28 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:plannerapp/widgets/chart.dart';
+import 'package:plannerapp/widgets/new_transaction.dart';
+import 'package:plannerapp/widgets/transaction_list.dart';
+
+import './widgets/new_transaction.dart';
+import './widgets/transaction_list.dart';
+import './models/transaction.dart';
+import './widgets/chart.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Personal expenses',
+      theme: ThemeData(
+        primarySwatch: Colors.lightGreen
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  // String titleInput;
+  // String amountInput;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _userTransactions = [
+    /*Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),*/
+  ];
+  List<Transaction> get _recentTransactions{
+    return _userTransactions.where((tx){
+      return tx.date.isAfter(DateTime.now().subtract(Duration(days: 7)),);
+    }).toList();
+
+  }
+
+  void _addNewTransaction(String txTitle, double txAmount ,DateTime chosenDate) {
+    final newTx = Transaction(
+      title: txTitle,
+      amount: txAmount,
+      date: chosenDate,
+      id: DateTime.now().toString(),
+    );
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+      context: ctx,
+      builder: (_) {
+        return GestureDetector(
+          onTap: () {},
+          child: NewTransaction(_addNewTransaction),
+          behavior: HitTestBehavior.opaque,
+        );
+      },
+    );
+  }
+  void _deleteTransaction (String id){
+    setState(() {
+      _userTransactions.removeWhere((tx){
+        return tx.id == id;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Personal Expenses'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () => _startAddNewTransaction(context),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+          Chart(_recentTransactions),
+            TransactionList(_userTransactions, _deleteTransaction),
+
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _startAddNewTransaction(context),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/chart_bar.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+ class ChartBar extends  StatelessWidget{
+   final String label;
+   final double spendingAmount;
+   final double spendingPctOfTotal;
+   ChartBar(this.label,this.spendingAmount,this.spendingPctOfTotal);
+   @override
+    Widget build(BuildContext  context){
+    return Column(children:<Widget> [
+      Container(
+        height: 20,
+        child: FittedBox(
+          child:Text('₹'+spendingAmount.toStringAsFixed(0)),
+        ),
+      ),
+
+     SizedBox(height: 4,),
+     Container(
+      height: 60,
+      width: 10,
+      child:  Stack(children: <Widget>[
+       Container(decoration: BoxDecoration(
+           border: Border.all(
+               color: Colors.lightGreenAccent,width: 1.0),
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(20),
+       ),
+       ),
+        FractionallySizedBox(
+          heightFactor: spendingPctOfTotal,
+          child: Container(decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              borderRadius: BorderRadius.circular(20),
+          ),
+          ),)
+      ],),
+     ),
+     SizedBox(height: 4,),
+     Text(label),
+    ],);
+
+
+  }
+}
